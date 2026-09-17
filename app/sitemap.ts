85f5947cda92c73a0ef7fcd4d6dfb39a788bd599
Daramola Joseph Omoyele <1978dju@gmail.com>
2026-09-17 02:12:43 +0100

    Add new heritage pages to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -10,7 +10,8 @@ import { getArchiveItems } from "@/lib/data/archive";
 const staticRoutes = [
   "/", "/our-story", "/heritage", "/heritage/faith", "/heritage/faith/first-baptist-church",
   "/heritage/agado", "/heritage/ate", "/heritage/agbagba-ide", "/heritage/takete-ide-anthem",
-  "/heritage/traditional-institution", "/heritage/traditional-marriage", "/takete-ide-day",
+  "/heritage/traditional-institution", "/heritage/traditional-marriage", "/heritage/festivals",
+  "/heritage/pacesetters", "/heritage/community-organisations", "/takete-ide-day",
   "/development", "/our-people", "/news", "/events", "/gallery", "/weather", "/diaspora", "/archive",
   "/archive/oral-history", "/tipu", "/tipu/branches", "/tipu/branches/lokoja",
   "/tipu/branches/ilorin", "/diaspora/uk-europe", "/takete-ide-day/cultural-attire", "/centenary", "/support",
